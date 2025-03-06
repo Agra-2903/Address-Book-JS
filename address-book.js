8d156a4ec05f97bc31cf59530241196b92315944
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-06 11:49:52 +0530

    [Kushagra] Add. (Address Book) Deleting contact program in UC5

--- address-book.js
+++ address-book.js
@@ -49,3 +49,8 @@ function editContact(name, updatedContact) {
         console.log("Contact Not Found!");
     }
 }
+
+// UC5: Ability to delete a contact by name
+function deleteContact(name) {
+    addressBook = addressBook.filter(contact => contact.firstName !== name);
+}
